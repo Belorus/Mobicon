abf8c559de70499acb91c1bb6f58f35b022f66d7
Grigory Perepechko <grigoryp@playtika.com>
2018-09-17 17:50:56 +0300

    Fixes

--- Mobicon/Pages/Segments.cshtml.cs
+++ Mobicon/Pages/Segments.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Mobicon.Models;
+using Mobicon.Services;
 
 namespace Mobicon.Pages
 {
@@ -12,12 +13,16 @@ namespace Mobicon.Pages
     public class SegmentsModel : PageModel
     {
         private readonly DataContext _dataContext;
+        private readonly ImportManager _importManager;
 
         public Segment[] Segments { get; private set; }
 
-        public SegmentsModel(DataContext dataContext)
+        public SegmentsModel(
+            DataContext dataContext,
+            ImportManager importManager)
         {
             _dataContext = dataContext;
+            _importManager = importManager;
         }
 
         public void OnGet()
@@ -37,6 +42,57 @@ namespace Mobicon.Pages
             _dataContext.SaveChanges();
 
             return RedirectToPage();
+        }
+
+        public IActionResult OnPostImport(int id, string name, string data)
+        {
+            var entries = _importManager.ImportYaml(data, User.Identity.Name);
+
+            var config = new Config()
+            {
+                Name = name,
+                SegmentId = id,
+                Entries = entries.ToList(),
+                CreatedAt = DateTime.Now,
+                CreatedBy = User.Identity.Name,
+                UpdatedAt = DateTime.Now,
+                UpdatedBy = User.Identity.Name
+            };
+
+            _dataContext.Configs.Add(config);
+
+            _dataContext.SaveChanges();
+
+            return RedirectToPage("Config", new { id = config.Id });
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            if (User.IsInRole(UserRole.Admin.ToString()))
+            {
+                _dataContext.Segments.Remove(_dataContext.Segments.Find(id));
+                _dataContext.SaveChanges();
+            }
+
+            return RedirectToPage("Segments");
+        }
+
+        public IActionResult OnPostAddConfig(int id, string configName)
+        {
+            var config = new Config
+            {
+                Name = configName,
+                SegmentId = id,
+                CreatedAt = DateTime.Now,
+                CreatedBy = User.Identity.Name,
+                UpdatedAt = DateTime.Now,
+                UpdatedBy = User.Identity.Name
+            };
+            _dataContext.Configs.Add(config);
+
+            _dataContext.SaveChanges();
+
+            return RedirectToPage("Config", new {id = config.Id});
         }
     }
 }
